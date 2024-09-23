5155284225bb4ae2c9639a098ffc98652f43d189
kadev <karimaoudiaa@gmail.com>
2024-09-23 17:39:47 +0200

    ✨ F01: Login>create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
+import LoginPages from './components/pages/LoginPages'
 
 function App() {
 
 
   return (
-
-      <div>
-      </div>
+<LoginPages/>
 
   )
 }
---
+++ src/components/pages/LoginPages.jsx
@@ -0,0 +1,4 @@
+function LoginPages() {
+  return <div>LoginPages</div>;
+}
+export default LoginPages;
