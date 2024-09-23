6d0e7d3c142f8f4e7addc58ec7821d386d7fb1d3
kadev <karimaoudiaa@gmail.com>
2024-09-23 18:58:44 +0200

    ✨ F01: Login>add title /form

--- src/components/pages/LoginPages.jsx
+++ src/components/pages/LoginPages.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+
 function LoginPages() {
-  return <div>LoginPages</div>;
+  // state
+  const [inputValue, setInputValue] = useState("bob");
+  // comportement
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    setInputValue("");
+  };
+  const handelChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  //affichage (render)
+
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connecter-vous</h2>
+      <form action="submit" onSubmit={handelSubmit}>
+        <input
+          value={inputValue}
+          onChange={handelChange}
+          type="text"
+          placeholder="Entre votre prénom.."
+          required
+        />
+        <button>Accéder à votre espace </button>
+      </form>
+    </div>
+  );
 }
 export default LoginPages;
